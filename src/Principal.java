3b1b91208e641a55e55391d5df8eca5bddf9c829
ericrc77 <ericzezim@gmail.com>
2023-05-02 12:24:29 -0300

    Initial commit

---
+++ src/Principal.java
@@ -0,0 +1,24 @@
+import br.com.alura.screenmatch.modelos.Filme;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Filme meuFilme = new Filme();
+        meuFilme.setNome("O poderoso chefão");
+        meuFilme.setAnoDeLancamento(1970);
+        meuFilme.setDuracaoEmMinutos(180);
+
+
+        meuFilme.exibeFichaTecnica();
+        meuFilme.avalia(8);
+        meuFilme.avalia(10);
+        meuFilme.avalia(5);
+        System.out.println(meuFilme.pegaMedia());
+
+        System.out.println("total de avaliações " + meuFilme.getTotalDeAvaliacoes());
+
+
+
+
+    }
+}
